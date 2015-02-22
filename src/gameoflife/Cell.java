1dd14ba572b6acaa7380c62a3e4faa08951bb363
Andrew <werdnaonaps@gmail.com>
2015-02-22 03:26:53 -0500

    deleted neighbors field from Cell class

--- src/gameoflife/Cell.java
+++ src/gameoflife/Cell.java
@@ -3,11 +3,9 @@ package gameoflife;
 import java.util.ArrayList;
 
 public class Cell {
-	Cell[] neighbors; //array representing eight neighboring cells
 	boolean isAlive; //calculates whether cell is alive
 	
 	public Cell() {
-		neighbors = new Cell[8];
 		isAlive = false;
 	}
 	
