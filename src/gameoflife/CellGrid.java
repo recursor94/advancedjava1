75c37b346483c87fc02022439e9a9c6431f0cde5
Andrew <werdnaonaps@gmail.com>
2015-02-22 03:24:45 -0500

    added default constructor to cell grid

--- src/gameoflife/CellGrid.java
+++ src/gameoflife/CellGrid.java
@@ -4,7 +4,13 @@ public class CellGrid {
 	Cell[][] grid; //grid representing rows and columns of cells
 	
 	public CellGrid() {
+		grid = new Cell[25][25];  //for this project, grid will be 25x25
 		
+		for(int i = 0; i < grid.length; i++) {
+			for(int j = 0; j < grid[0].length; j++) {
+				grid[i][j] = new Cell();
+			}
+		}
 	}
 
 }
