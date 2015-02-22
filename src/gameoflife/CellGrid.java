42be67395f0e656765d2984162b4b6bc1dff9cf6
Andrew <werdnaonaps@gmail.com>
2015-02-22 11:06:24 -0500

    created get index of cell function for the grid

--- src/gameoflife/CellGrid.java
+++ src/gameoflife/CellGrid.java
@@ -21,5 +21,36 @@ public class CellGrid {
 	public void setGrid(Cell[][] grid) {
 		this.grid = grid;
 	}
+	
+	public boolean cellisOnTopBorder(Cell cell) {
+		/*Determines whether the cell is on the top border
+		 * in which case it's neighbors must be the cells on the bottom border
+		 */
+		
+		
+		return false;
+	}
+	
+	public int [] getIndexOfCell(Cell cell) { 
+		/* arrays by default can not return the index of an element
+		 * which is necessary in this program
+		 */
+	
+		int i =0, j = 0;
+		while(grid[i][j] != cell) {
+			i++;
+			j++;
+		}
+		
+		int index[] = new int[2];
+		index[0] = i;
+		index[1] = j;
+		return index;
+		
+		//TODO: Need to add test case for if the element is not in the grid, which should actually never happen
+		
+	}
+	
+	 
 
 }
