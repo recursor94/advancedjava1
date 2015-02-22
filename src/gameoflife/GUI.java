b35e796f248201c46231487519ee73b3a2fe8102
Andrew <werdnaonaps@gmail.com>
2015-02-22 10:42:06 -0500

    made button actionlistener initialize with reference to button and cell and created behavior for the action performed method

--- src/gameoflife/GUI.java
+++ src/gameoflife/GUI.java
@@ -1,6 +1,8 @@
 package gameoflife;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,19 +16,46 @@ public class GUI extends JFrame {
 		//display grid of cells in grid form, representing each cell as a button in the gui
 		for(int i = 0; i< cellGrid.getGrid().length; i++) {
 			for(int j = 0; j < cellGrid.getGrid()[0].length; j++) {
-				
+				JButton cellButton = null;
 				if(cellGrid.getGrid()[i][j].isAlive()) {  /*in the current setup, this check will always return false, 
-														**But, it makes sense, and if another programmer ever decides to set cells to be alive by default, this will work as expected*/
-					add(new JButton("0"));
+												**But, it makes sense, and if another programmer ever decides to set cells to be alive by default, this will work as expected*/
+					 cellButton = new JButton("0");
+					
 				}
 				
 				else{
-					add(new JButton(""));   
+					 cellButton = new JButton("-");
+					
 				}
+				add(cellButton);
+				cellButton.addActionListener(new CellButtonListener(cellButton, cellGrid.getGrid()[i][j]));
 			}
 		}
 		pack();
 		setVisible(true);
+	}
+	
+	private class CellButtonListener implements ActionListener {
+		private JButton cellButton;
+		private Cell cell;
+		private CellButtonListener(JButton cellButton, Cell cell) {
+			this.cellButton = cellButton;
+			this.cell = cell;
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(cell.isAlive()) { //uninitialize cell if cell is alive and user clicks on it during pre-game initialization
+				cell.setAlive(false);
+				cellButton.setText("-");
+			}
+			else {
+				cell.setAlive(true);
+				cellButton.setText("0"); //set cell to alive if the user clicks on it during pre-game initialization
+				pack();
+			}
+			
+		}
+		
 	}
 	
 	public static void main(String[] args) {
