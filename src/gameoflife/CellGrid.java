f12cef6f6fa087d69b3d4a61e23990279b87559e
Andrew <werdnaonaps@gmail.com>
2015-02-22 03:18:48 -0500

    Created three classes to represent the cell, cell grid, and game gui

---
+++ src/gameoflife/CellGrid.java
@@ -0,0 +1,10 @@
+package gameoflife;
+
+public class CellGrid {
+	Cell[][] grid; //grid representing rows and columns of cells
+	
+	public CellGrid() {
+		
+	}
+
+}
---
+++ src/gameoflife/GUI.java
@@ -0,0 +1,12 @@
+package gameoflife;
+
+import javax.swing.JFrame;
+
+public class GUI extends JFrame {
+	CellGrid cellGrid;
+	
+	public GUI() {
+		cellGrid = new CellGrid();
+	}
+
+}
